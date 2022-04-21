7a85de995b226739a137152e63feb4856bf8a59e
Tien Long Lam <lamtienlong9@gmail.com>
2022-04-21 17:46:20 +1000

    Pickers and name text field

--- Bubble Pop/Controller/SettingViewController.swift
+++ Bubble Pop/Controller/SettingViewController.swift
@@ -1,14 +1,53 @@
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var bubbleCountLabel: UILabel!
     @IBOutlet weak var minutePicker: UIPickerView!
     @IBOutlet weak var secondPicker: UIPickerView!
     
+    let minutes = Array(0...5)
+    let seconds = Array(0...59)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        minutePicker.dataSource = self
+        minutePicker.delegate = self
+        secondPicker.dataSource = self
+        secondPicker.delegate = self
+        nameTextField.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == minutePicker {
+            return minutes.count
+        }
+        else if pickerView == secondPicker {
+            return seconds.count
+        }
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        if pickerView == minutePicker {
+            return String(minutes[row]) + " min"
+        }
+        else if pickerView == secondPicker {
+            return String(seconds[row]) + " sec"
+        }
+            return ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
     
     @IBAction func bubbleCountSliderValueChanged(_ sender: UISlider) {
@@ -24,5 +63,6 @@ class SettingViewController: UIViewController {
              */
         }
     }
+    
   
 }
